5cb63d86a9ec0f0083687ef70f67d312696e4222
JeffNJellyBean <54992195+JeffNJellyBean@users.noreply.github.com>
2019-09-10 15:18:12 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+class Game {
+  constructor() {
+    this.init();
+  }
+  
+  init() {
+    var jquery = document.createElement('script');
+    jquery.src = 'js/jquery.js';
+    document.body.appendChild(jquery);
+    
+    this.iteration = [];
+    this.story();
+  }
+  
+  story() {
+    this.storyline = {
+      'You are in a clearing.\nYou are in a clearing, and you’re alone.\nYou are in a clearing, and the path leads left or right.': [
+        '
+       ]
+    }
+  }
+  
+  play() {
+    
+  }
+}
 
+var game = new Game();
+game.init();
