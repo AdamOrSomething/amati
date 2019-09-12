4ca45cf8ab4be95a7c43312f660d843e61c14a6f
JeffNJellyBean <54992195+JeffNJellyBean@users.noreply.github.com>
2019-09-12 15:18:24 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,15 +13,31 @@ class Game {
   }
   
   story() {
-    this.storyline = {
-      'You are in a clearing.\nYou are in a clearing, and you’re alone.\nYou are in a clearing, and the path leads left or right.': [
-        '
-       ]
-    }
+    this.storyline = [
+      'You are in a clearing.\nYou are in a clearing, and you\'re alone.\nYou are in a clearing, and the path leads left or right.' +
+      'CHOICESLeft:Right',
+      {
+        
+      }
+    ];
   }
   
   play() {
+    $('select').empty();
     
+    var current_storyline = this.storyline;
+    for(i=0;i<this.iteration.length;i++) {
+      current_storyline = current_storyline[this.iteration[i]];
+    }
+    var split = current_storyline.split('CHOICES');
+    var text = split[0];
+    var options = split[1].split(':');
+    
+    var choice;
+    $('#output').text(text);
+    $.each(options, function(index, value) {
+      choice = document.createElement('option');
+    });
   }
 }
 
