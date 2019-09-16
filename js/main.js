37a335e0f9439dbfe8d5886c16cac1bc3a03a916
JeffNJellyBean <54992195+JeffNJellyBean@users.noreply.github.com>
2019-09-16 15:01:43 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,37 +2,33 @@ class Game {
   constructor() {
     this.init();
   }
-  
+
   init() {
-    var jquery = document.createElement('script');
-    jquery.src = 'js/jquery.js';
-    document.body.appendChild(jquery);
-    
     this.iteration = [0];
     this.story();
   }
-  
+
   story() {
     this.storyline = [
       'You are in a clearing.\nYou are in a clearing, and you\'re alone.\nYou are in a clearing, and the path leads left or right.' +
       'CHOICESLeft:Right',
       {
-        
+
       }
     ];
   }
-  
+
   play() {
     $('select').empty();
-    
+
     var current_storyline = this.storyline;
-    for(i=0;i<this.iteration.length;i++) {
+    for (i = 0; i < this.iteration.length; i++) {
       current_storyline = current_storyline[this.iteration[i]];
     }
     var split = current_storyline.split('CHOICES');
     var text = split[0];
     var options = split[1].split(':');
-    
+
     var choice;
     $('#output').text(text);
     $.each(options, function(index, value) {
@@ -43,9 +39,11 @@ class Game {
   }
 }
 
-window.onerror = function(message) {
-  alert(message);
-}
+$(document).ready(function() {
+  window.onerror = function(message) {
+    alert(message);
+  }
 
-var game = new Game();
-game.init();
+  var game = new Game();
+  game.init();
+});
