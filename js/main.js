9d4fed603e78c4d4f8be46421d07164cdbba5a77
JeffNJellyBean <54992195+JeffNJellyBean@users.noreply.github.com>
2019-10-10 15:17:49 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -54,7 +54,59 @@ class Game {
       'CHOICESLeft:Right',
       [
         [
-          ''
+          `
+You follow the path until it leads out of the forest, up a hill, and the ensuing sight leaves you breathless.
+It's a skyline. Tall, slender buildings rise in the distance, jagged and tapered to a point high above the clouds. They seem to  pierce the clouds, their height obscene, as if attempting to offend nature with their pure excess of elevation. They look as if someone broke a mirror and stood the shards upright.
+It’s beautiful. You walk down, dazed, into a street full of people. After hours in a forest, the sight of so many people throws you off for a second and you stand there in a trance until someone bumps into your shoulder, throwing you down. He apologizes quickly and, with an upwards gesture, he drops his briefcase in midair. You automatically hold your hand out to stop it, but to your shock it doesn’t fall. It just… stands there in midair, suspended by an unseen force. The man misinterprets your shock and replies good-naturedly “Hey, not all businessmen are corrupt and greedy.”
+CHOICESHmm Suspicion:No Suspicion
+          `,
+          [
+            [
+              `
+He smiles, but something’s off about it. You get up, and he leans forward. His eyes, once full of humor, now betray nothing. You can’t detect any emotion. He leans forward, over your shoulder, hissing into your ear.
+“----------------?”
+“What?”
+You feel pressure behind your ear, and you collapse.
+
+“You’re absolutely sure it’s him.”
+“Yes.”
+“Caaarl…”
+“I saw this man with my own eyes that night. I’m absolutely sure it’s him.”
+“And how did he respond to the query?”
+You peek through the tiniest crack you can possibly create with your eyelids. You’re on a metal slab, restrained by metal cuffs welded deep into the slab. Monitors displaying things you don’t understand surround you. The slab is slightly elevated, about a foot off the floor, and angled so you’re in a semi-upright position, feet pointed at the two men in front of you. They’re facing each other in deep conversation. They’re wearing ---, with ---. One of them is tall and thin, with short blond hair. The other is slightly shorter, with long and black hair tied back in a ponytail. Suddenly one of the monitors starts beeping rapidly. They spin around, look at you. Blond walks over and examines your restraints, and Ponytail sneers at you menacingly. “Look who’s awake,'' says Blond. “Where am I?!” You say, hysteria starting to rise in your chest. “How’d I get here? Who are you people?”
+Blond grins. “Welcome,” he says, “To Area 77!”
+Ponytail snickers.
+“What my friend’s trying to say is, this place has a name that’s heavily classified. The mere knowledge of it is a capital offense, and being in it without clearance…”
+Blond draws his finger across his throat and hangs his head backward limply. 
+You don’t know how to respond to that.CHOICESFight against your restraints:Play along and wait
+              `,
+              [
+                [
+                  // TODO
+                  `
+Before you do, a monitor to your right beeps, as if announcing your intent. Then you thrash wildly around, but you don’t gain an inch of give. The cuffs tighten around your wrists. Blond smirks. “Ah, so you’ve triggered Gerald.”
+“What?”
+Ponytail pokes the monitor that beeped. “This is Gerald. He will beep when your heart pressure rises substantially, making him a good indicator of when you’re lying, nervous or about to do something.”
+“So don’t,” Blond finishes.
+                  `,
+                  [
+                    [],
+                    []
+                  ]
+                ],
+                [
+                  // TODO
+                  `
+“So let me guess, I don’t leave this place alive?” you say with a nervous chuckle.
+Ponytail claps sarcastically, and Blond smiles mockingly. “Ah!  He gets it!  You’re a smart one, ain’tcha?” he says. If they were a bit less psycho, they could make a half-decent comedy duo, you think.  
+Blond’s expression hardens.  He raises a pistol to your face, seemingly out of nowhere.
+Your heart stops.
+                  `
+                ]
+              ]
+            ],
+            []
+          ]
         ],
         [
           `
